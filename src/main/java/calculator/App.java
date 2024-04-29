67b5727b2f2699ba328600266bffbcd37dbfb949
Robbie <robbie331@>
2024-04-30 02:39:17 +0900

    level_1 - 요구사항-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -10,11 +10,26 @@ public class App {
         System.out.println("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt();
 
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        char ch1 = sc.next().charAt(0);
 
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
-        System.out.println("num1, num2 = " + num1 + ", " + num2);
+        int result = 0;
+        if(ch1=='+') {
+            result = num1 + num2;
+        }else if(ch1=='-'){
+            result = num1 - num2;
+        }else if(ch1=='*'){
+            result = num1 * num2;
+        }else if(ch1=='/'){
+            result = num1 / num2;
+        }
+
+
+        System.out.println("결과 : " + result);
+
 
     }
 }
