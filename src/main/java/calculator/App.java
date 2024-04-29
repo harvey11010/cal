c3513129b312d185db0b118410895a48f752f787
Robbie <robbie331@>
2024-04-30 02:13:52 +0900

    1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,14 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+
+        System.out.println("첫 번째 숫자를 입력하세요: ");
     }
 }
