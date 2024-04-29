a4c95cf749bac2df0a3750b17e10803c3234254e
Robbie <robbie331@>
2024-04-30 02:48:54 +0900

    level_1 - 요구사항-3, 4

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,6 +6,7 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        while(true){
 
         System.out.println("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt();
@@ -15,6 +16,7 @@ public class App {
 
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
+
 
         int result = 0;
         if(ch1=='+') {
@@ -30,6 +32,15 @@ public class App {
 
         System.out.println("결과 : " + result);
 
+        sc.nextLine();
 
+        System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) ");
+        String exit = sc.nextLine();
+        if(exit.equals("exit")){
+            break;
+        }
+
+
+    }
     }
 }
