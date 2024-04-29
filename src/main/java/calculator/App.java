14ccba25919bfe479c531c55752e0875db3e14cf
Robbie <robbie331@>
2024-04-30 02:55:42 +0900

    level_1 - 요구사항-5

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,6 +6,9 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] results = new int[10]; // 결과를 저장할 배열 선언 및 생성
+        int count = 0; // 저장된 결과의 개수를 세는 변수
+        int index = 0;
         while(true){
 
         System.out.println("첫 번째 숫자를 입력하세요: ");
@@ -28,16 +31,21 @@ public class App {
         }else if(ch1=='/'){
             result = num1 / num2;
         }
-
+        results[count] = result;
+        count++;
 
         System.out.println("결과 : " + result);
 
         sc.nextLine();
 
-        System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) ");
+        System.out.println("더 계산하시려면 엔터를 눌러주세요 (exit 입력 시 종료) ");
         String exit = sc.nextLine();
         if(exit.equals("exit")){
             break;
+        }
+        System.out.println("저장된 결과:");
+        for(int i = 0; i < count; i++) {
+            System.out.println(results[i]);
         }
 
 
