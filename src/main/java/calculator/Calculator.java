5458f2670e5890f4c05634794fc81a12ff69c43e
Robbie <robbie331@>
2024-05-03 07:55:44 +0900

    개인과제 2-1 throw 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,40 @@
 package calculator;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
+
+
+    List<Integer> results = new ArrayList<>();
+
+    public int Calculate(int num1, int num2, char operator) {
+        int result = 0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+              if(num2 != 0) {
+                 result = num1 / num2;
+              }else{
+                  throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+              }
+
+                break;
+            default:
+                System.out.println("올바른 선택이 아닙니다.");
+                throw new UnsupportedOperationException("올바른 선택이 아닙니다.");
+        }
+        return result;
+    }
 
 }
